7dead11eef31e45e4664df9c6ac2032042f705d9
Hannah Angle <hannahcangle@gmail.com>
2020-08-11 22:46:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,14 @@
 class Owner
+ attr_accessor :name, :pets 
+ attr_reader :species
+ 
+ def initialize(species)
+   @species = "human"
+ end 
+ 
+ def say_species 
+   @species = species 
+ end 
  
  
 end
